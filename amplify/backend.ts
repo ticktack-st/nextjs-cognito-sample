9edd0b384987c06787bf3ac345a3204262653b32
ticktack-st <moglandwrapper@gmail.com>
2025-04-24 16:05:14 +0900

    add: data

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,10 +1,10 @@
 import { defineBackend } from '@aws-amplify/backend';
 // import { auth } from './auth/resource.js';
-// import { data } from './data/resource.js';
+import { data } from './data/resource';
 
 
 
 defineBackend({
   // auth,
-  // data,
+  data,
 });
