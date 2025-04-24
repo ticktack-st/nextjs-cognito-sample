135d7d64463abf009e855bfbd82c53f42e9253f7
ticktack-st <moglandwrapper@gmail.com>
2025-04-24 17:40:52 +0900

    del: not enouph

--- app/todo-list/page.tsx
+++ app/todo-list/page.tsx
@@ -1,19 +1,19 @@
 'use client'
 
-import type { Schema } from '../../amplify/data/resource'
-import { generateClient } from 'aws-amplify/data'
+// import type { Schema } from '../../amplify/data/resource'
+// import { generateClient } from 'aws-amplify/data'
 
-const client = generateClient<Schema>()
+// const client = generateClient<Schema>()
 
 export default function TodoList() {
-  const createTodo = async () => {
-    await client.models.Todo.create({
-      content: window.prompt("Todo content?"),
-      isDone: false
-    })
-  }
+  //   const createTodo = async () => {
+  //     await client.models.Todo.create({
+  //       content: window.prompt("Todo content?"),
+  //       isDone: false
+  //     })
+  //   }
 
-  return <div>
-    <button onClick={createTodo}>Add new todo</button>
-  </div>
+  //   return <div>
+  //     <button onClick={createTodo}>Add new todo</button>
+  //   </div>
 }
