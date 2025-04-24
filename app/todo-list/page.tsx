3c303ecdf263bc76b327fc0d9ca35fc3e302ff0d
ticktack-st <moglandwrapper@gmail.com>
2025-04-24 17:00:30 +0900

    add: amplify_configure

--- app/todo-list/page.tsx
+++ app/todo-list/page.tsx
@@ -2,6 +2,10 @@
 
 import type { Schema } from '../../amplify/data/resource'
 import { generateClient } from 'aws-amplify/data'
+import { Amplify } from "aws-amplify";
+import outputs from "@/amplify_outputs.json";
+
+Amplify.configure(outputs);
 
 const client = generateClient<Schema>()
 
