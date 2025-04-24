f020ae21e749244575b608f956fdf1e4e3040eb0
ticktack-st <moglandwrapper@gmail.com>
2025-04-24 17:13:50 +0900

    fix: configure

--- app/todo-list/page.tsx
+++ app/todo-list/page.tsx
@@ -3,7 +3,7 @@
 import type { Schema } from '../../amplify/data/resource'
 import { generateClient } from 'aws-amplify/data'
 import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
+import outputs from "../../amplify_outputs.json";
 
 Amplify.configure(outputs);
 
