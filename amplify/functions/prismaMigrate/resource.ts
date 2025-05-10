4fef82d766c3925bd0b8feab30cf2b942a657023
ticktack-st <moglandwrapper@gmail.com>
2025-05-10 17:34:52 +0900

    fix

--- amplify/functions/prismaMigrate/resource.ts
+++ amplify/functions/prismaMigrate/resource.ts
@@ -1,4 +1,5 @@
 // import { defineFunction } from '@aws-amplify/backend';
+import * as path from 'path';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Runtime } from "aws-cdk-lib/aws-lambda";
 // import * as ec2 from 'aws-cdk-lib/aws-ec2';
@@ -60,7 +61,7 @@ export function defineCustomFunction({ stack }: { stack: Stack }) {
   const securityGroup = SecurityGroup.fromSecurityGroupId(stack, 'ExistingSG', 'sg-0738e478ff2be7ccb'); // ご自身のセキュリティグループ ID に置き換えてください
 
   const prismaMigrate = new NodejsFunction(stack, 'prismaMigrate', {
-    entry: './handler.ts',
+    entry: path.join(__dirname, 'handler.ts'), // 絶対パスを指定
     handler: 'handler',
     runtime: Runtime.NODEJS_22_X,
     timeout: Duration.seconds(20),
@@ -87,7 +88,7 @@ export function defineCustomFunction({ stack }: { stack: Stack }) {
           ];
         },
       },
-      forceDockerBundling: false,
+      forceDockerBundling: true,
     },
   });
   return prismaMigrate;
