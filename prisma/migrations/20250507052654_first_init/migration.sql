1305198154f2641102248eeea31b48b50d5ad58b
ticktack-st <moglandwrapper@gmail.com>
2025-05-10 23:36:38 +0900

    fix: sql

--- prisma/migrations/20250507052654_first_init/migration.sql
+++ prisma/migrations/20250507052654_first_init/migration.sql
@@ -1,9 +1,9 @@
 -- CreateTable
-CREATE TABLE "Post" (
-    "id" int NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "title" VARCHAR NOT NULL,
-    "content" VARCHAR,
-    "published" BOOLEAN NOT NULL,
-    "createdAt" DATETIME NOT NULL,
-    "updatedAt" DATETIME NOT NULL
+CREATE TABLE Post (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content VARCHAR(255) NOT NULL,
+    published BOOLEAN NOT NULL,
+    createdAt DATETIME NOT NULL,
+    updatedAt DATETIME NOT NULL
 );
