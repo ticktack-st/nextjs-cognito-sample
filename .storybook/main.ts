6893fbdf395c58e17a815214c8d090e9eb55898f
ticktack-st <moglandwrapper@gmail.com>
2025-05-26 17:38:20 +0900

    add: storybook sample test

---
+++ .storybook/main.ts
@@ -0,0 +1,19 @@
+import type { StorybookConfig } from '@storybook/experimental-nextjs-vite'
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@chromatic-com/storybook',
+    '@storybook/experimental-addon-test',
+  ],
+  framework: {
+    name: '@storybook/experimental-nextjs-vite',
+    options: {},
+  },
+  core: {
+    disableTelemetry: true, // Disable telemetry to avoid sending data to Storybook
+  },
+}
+export default config
