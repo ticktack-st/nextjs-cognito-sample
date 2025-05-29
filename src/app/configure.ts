da55e3d9e9821c4c8a44ab812a43d88ea50d572e
ticktack-st <moglandwrapper@gmail.com>
2025-05-29 11:05:51 +0900

    del: 設定情報

--- src/app/configure.ts
+++ src/app/configure.ts
@@ -3,9 +3,9 @@ import type { ResourcesConfig } from 'aws-amplify'
 export const configPoolA: ResourcesConfig = {
   Auth: {
     Cognito: {
-      userPoolId: 'ap-northeast-1_fVGVH0WsZ',
-      userPoolClientId: '1d27eld0o3b5onl981729npi0s',
-      // identityPoolId: "ap-northeast-1:bb11347a-a89d-42c5-a6f3-078322d19843",
+      userPoolId: '',
+      userPoolClientId: '',
+      // identityPoolId: "",
       // signUpVerificationMethod: "code",
       // userAttributes: {
       //   email: {
@@ -15,8 +15,7 @@ export const configPoolA: ResourcesConfig = {
       // allowGuestAccess: true,
       loginWith: {
         oauth: {
-          domain:
-            'ap-northeast-1fvgvh0wsz.auth.ap-northeast-1.amazoncognito.com',
+          domain: '<YOUR_DOMAIN>.auth.ap-northeast-1.amazoncognito.com',
           scopes: ['email', 'openid'],
           redirectSignIn: ['http://localhost:3000/logged-in'],
           redirectSignOut: ['http://localhost:3000'],
@@ -30,12 +29,11 @@ export const configPoolA: ResourcesConfig = {
 export const configPoolB: ResourcesConfig = {
   Auth: {
     Cognito: {
-      userPoolId: 'ap-northeast-1_h59Uj2q34',
-      userPoolClientId: '7n84i7ncvb3096rq07q3pe4fk1',
+      userPoolId: '<YOUR_USER_POOL_ID>',
+      userPoolClientId: '<YOUR_USER_POOL_CLIENT_ID>',
       loginWith: {
         oauth: {
-          domain:
-            'ap-northeast-1h59uj2q34.auth.ap-northeast-1.amazoncognito.com',
+          domain: '<YOUR_DOMAIN>.auth.ap-northeast-1.amazoncognito.com',
           scopes: ['openid'],
           redirectSignIn: ['http://localhost:3000'],
           redirectSignOut: ['http://localhost:3000'],
